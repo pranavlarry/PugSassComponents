9c5f12c7aacf070d26814191a90c568bcfb0224e
pranavlarry <pranavlarry111@gmail.com>
2019-07-26 14:31:39 +0530

    5 components

--- js/demo.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(e){
-    parallax();
-  });
-
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.jumbotron').css('top',(scrolled*0.0210)+'rem');
-  };
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(e){
+    parallax();
+  });
+
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.jumbotron').css('top',(scrolled*0.0150)+'rem');
+    $('.jumbotron > .jumbotron-content').css('top',-(scrolled*-0.035)+'rem');
+    $('.jumbotron > .jumbotron-content').css('opacity',1-(scrolled*.00175));
+  };
